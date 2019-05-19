878ce632cb834e1f8d4409165582cb09c97083d3
RShalihin RS <48857186+RShalihinRS@users.noreply.github.com>
2019-05-19 20:39:45 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,205 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Css Intregration -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Font Awesome Intregration-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>PHD to HTML | Creat PHD to HTML with WD Shawon</title>
+</head>
+
+<body>
+  
+
+    <?php include('components/header.php'); ?>
+
+
+    <!-- first container -->
+
+    <div class="bg1">
+        <h3>Corporate Maker</h3>
+        <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>
+            Aut aliquam libero tempore consequuntur</P>
+        <button class="btn btn-outline-primary">Contact Us</button>
+    </div> <!-- first container End -->
+
+
+    <!-- secend container -->
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4 col-sm-4 jumbotron text-center">
+                <i class="fa fa-briefcase fa-3x iteg"></i>
+                <h3 class=" text-center">1. BETTER PLANING</h3>
+                <p class=" text-center"> At first we are doing some plan for any project.
+                    Our project always to be easy, simple, and user friendly.
+                    We follow modern trend. </p>
+
+            </div>
+
+            <div class="col-md-4 col-sm-4 text-center mt-5">
+                <i class="fas fa-pencil-alt fa-3x mt-3 iteg"></i>
+                <h3 class=" text-center">2. MAKE ARCHITECTURE</h3>
+                <p class=" text-center">The architecture of a software system
+                    refers to an abstract system. Architecture is concerned
+                    with making sure the softwar </p>
+
+
+            </div>
+
+            <div class="col-md-4 col-sm-4 jumbotron text-center">
+                <i class="fa fa-building fa-3x iteg"></i>
+                <h3 class=" text-center">3. IMPLEMENTATION</h3>
+                <p class=" text-center">This is the final step , reducing a design
+                    to code may be the most obvious part of the software engineering
+                    job, but it is not largest</p>
+
+
+            </div>
+
+        </div>
+    </div>
+
+    <!-- 3rd container -->
+
+    <div class="container">
+        <div class="row">
+
+            <!-- Left Side Gride -->
+            <div class="col-md-4 col-sm-4 text-right box1">
+                <h3 class="text-primary mr-3">Why Corporate?</h3>
+                <p class="mr-3">Corporate is one of the most experienced agency.
+                    We serve to better service for our happy clients.
+                    And also strong to keep our promise.</p>
+
+            </div>
+
+            <!-- Right Side Gride -->
+            <div class="col-md-8 col-sm- text-left boximg">
+
+                <!-- Right Side Upper Row  -->
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <img src="images/user.png" alt="">
+                        <h3>User Friendly</h3>
+                        <p>In hac habitasse platea dictumst. Nunc sit amet leo eros.
+                            Praesent ultricies, eros tincidunt iaculis pellentesque
+
+                        </p>
+
+                    </div>
+                    <div class="col-md-6">
+                        <img src="images/responsive.png" alt="">
+                        <h3>Responsive Design</h3>
+                        <p>Curabitur ac mauris maximus, vulputate ligula feugiat,
+                            molestie erat. Mauris a est non diam sagittis tincidunt suscipit ut urna.
+                        </p>
+
+
+                    </div>
+
+                </div> <!-- Right Side Upper Row End  -->
+
+                <!-- Right Side Bottom Row  -->
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <img src="images/supported.png" alt="">
+                        <h3>24/7 Supported</h3>
+                        <p>An auctor, est eget efficitur lacinia, diam ante convallis nunc,
+                            a convallis tellus ex sed nulla. Sed efficitur tortor non lectus auctor</p>
+
+                    </div>
+                    <div class="col-md-6">
+                        <img src="images/money.png" alt="">
+                        <h3>Our Guarantee</h3>
+                        <P>Maecenas volutpat mi sed urna efficitur,
+                            mollis tristique quam eleifend. Mauris in luctus eros,
+                            sit amet ornare nisl.</P>
+
+                    </div>
+                </div> <!-- Right Side Bottom Row  End -->
+
+
+            </div> <!-- Right Side Gride End -->
+
+
+        </div>
+
+
+    </div> <!-- 3rd container End -->
+
+
+    <!-- 4th container -->
+
+    <div class="container-fluid cservice">
+        <i class="fa fa-life-ring fa-5x iteg2"></i>
+        <h2 class="text-white cservice1"><strong>24/7 customer service supported by professionals</strong></h2>
+        <p class="cservice1">Our team stands ready to assist you at all times. With multiple ways to get in touch,
+            including forums, ticket system, live chat and telephone, you will always find what you need.</p>
+
+    </div> <!-- 4rd container -->
+
+
+
+    <!-- 5th container -->
+
+    <div class="container-fluid oteam">  <!-- oteam class call -->
+       
+        <h2 class="text-center uppercase mb-5">OUR EXPERT TEAM</h2>
+        <p class="text-center mt-5">Here’s the MegaStar family. A small and reliable team with some excellent developer,
+            frontend engineer
+            swho are working hardto deliver colorful moments for your next project.</p>
+
+    </div> <!-- 5th container End -->
+
+
+    <!-- 6th Container Our Team -->
+
+    <?php include('components/ourteam.php'); ?>
+
+    <!-- 6th Container Our Team end -->
+
+
+    <!-- Footer top -->
+
+    <?php include('components/topfooter.php'); ?>
+
+    <!-- Footer top end -->
+
+
+    <!-- Footer Buttom -->
+ 
+    <?php include('components/btmfooter.php'); ?>
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
